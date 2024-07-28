0c4dcd4e97855f6b52c3122214135d611cca0155
Kacper Akdag-Ochnik <kacperochnik01@gmail.com>
2024-07-28 14:56:53 +0200

    input vector fix

--- src/Input.cpp
+++ src/Input.cpp
@@ -1,10 +1,11 @@
 #include "Input.h"
+#include "raymath.h"
 
 const Vector2 Input::JoystickVector()
 {
-    return Vector2{
+    return Vector2Normalize(Vector2{
         static_cast<float>(IsKeyDown(KEY_RIGHT) - IsKeyDown(KEY_LEFT)),
-        static_cast<float>(IsKeyDown(KEY_UP) - IsKeyDown(KEY_DOWN))};
+        static_cast<float>(IsKeyDown(KEY_DOWN) - IsKeyDown(KEY_UP))});
 }
 
 const bool Input::AButton() { return IsKeyDown(KEY_ONE); }
